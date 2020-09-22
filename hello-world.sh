78cea15ecb3ae692646c5f10be0beb49f58d1b24
uc-johns2br <johns2br@mail.uc.edu>
2020-09-22 07:13:02 -0400

    BRJ 09/22/2020 07:12 - Work on webpage

--- hello-world.sh
+++ hello-world.sh
@@ -1,1 +1,6 @@
-echo Hello World.
+#!/bin/bash
+echo "Content-type: text/html"
+echo ""
+echo "<html><head><title>Astrum Network Amalysis</title></head><body>"
+echo "Hello World. <br>"
+echo "</body></html>"
