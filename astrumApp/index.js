d9cba398bc5f79972a9a54f90bca5fc87f1b06e1
Brett Johnson <johns2br@mail.uc.edu>
2020-10-04 12:20:01 -0400

    BRJ 10/04/2020 12:19 - Added basic server functionality to index.js

--- astrumApp/index.js
+++ astrumApp/index.js
@@ -4,9 +4,15 @@
  * Required External Modules
  */
 
+const express = require("express");
+const path = require("path");
+
 /**
  * App Variables
  */
+
+const app = express();
+const port = process.env.PORT || "3000";
 
 /**
  *  App Configuration
@@ -16,6 +22,15 @@
  * Routes Definitions
  */
 
+app.get("/", (req, res) => {
+    res.status(200).send("Astrum Network Analysis");
+});
+
 /**
  * Server Activation
  */
+
+app.listen(port, () => {
+    console.log(`Listening to requests on http://localhost:${port}`);
+});
+
