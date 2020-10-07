a8604f312be12279d1a913e9376355fa143c6145
martzbm <70920878+martzbm@users.noreply.github.com>
2020-10-07 08:53:30 -0400

    BMM 10/07/2020 8:53am - Continued work on Linux remote script

--- linuxcommands.sh
+++ linuxcommands.sh
@@ -8,9 +8,21 @@
 
 #Variables from Astrum.sh can be passed as password, username, and hostname
 
+#BMM 10/7/2020 7:30am These are the remote commands that will run on the Linux device. They will be outputted to a temp file for further parsing.
+
 sshpass -p 'A5t7um' ssh root@zeropi-01.hpbd.uc.edu '
 
-echo "It worked"
+for i in $(usb-devices | awk -F":" '\''{'print $2'}'\'' | grep Manufacturer | grep -v =Linux); do usb-devices | grep -B 3 -A 4 $i;done
 
-df -h
-' 
+df -h 
+
+sestatus 
+
+firewall-cmd --state 
+
+awk -F: '\''{ print $1}'\'' /etc/passwd 
+
+' > device.temp
+
+#BMM 10/7/2020 8:47am I need to continue with parsing. If run against the zeropi's nothing will show for usb devcies because it is looking for the Manufactuer value of the usb device and only reporting on what is not a Linux device. This line is able to locate a webcam or mouse, for instance. Will test with personal raspberry pi.
+
