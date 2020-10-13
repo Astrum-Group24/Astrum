d3f5da4a601a8c47580d3beb5ae2dd8309f61c53
martzbm <70920878+martzbm@users.noreply.github.com>
2020-10-13 08:04:41 -0400

    BMM 10/13/2020 8:04am - Updated linuxcommands.sh

--- linuxcommands.sh
+++ linuxcommands.sh
@@ -6,22 +6,34 @@
 
 #BMM 10/6/2020 6:40am SSHPASS MUST BE INSTALLED ON ASTRUM
 
-#Variables from Astrum.sh can be passed as password, username, and hostname
-
-#BMM 10/7/2020 7:30am These are the remote commands that will run on the Linux device. 
+#BMM 10/7/2020 7:30am Variables from Astrum.sh can be passed as password, username, and hostname
 
 sshpass -p 'A5t7um' ssh -o stricthostkeychecking=no root@zeropi-01.hpbd.uc.edu '
 
+echo '\''<usb>'\''
 for i in $(usb-devices | awk -F":" '\''{print $2}'\'' | grep Manufacturer | grep -v =Linux); do usb-devices | grep -B 3 -A 4 $i;done 
+echo '\''</usb>'\''
 
+echo '\''<drivespace>'\''
 df -hP | grep -v Filesystem | awk '\''0+$5 >= 75  {print ;}'\''
+echo '\''</drivespace>'\''
 
+echo '\''<selinux>'\''
 sestatus 
+echo '\''</selinux>'\''
 
+echo '\''<firewalld>'\''
 firewall-cmd --state 
+echo '\''</firewalld>'\''
 
+echo '\''<iptables>'\''
+service iptables status
+echo '\''</iptables>'\''
+
+echo '\''<users>'\''
 awk -F: '\''{ print $1}'\'' /etc/passwd 
+echo '\''</users>'\''
 
 ' > device.txt
 
-#BMM 10/7/2020 3:23pm I need to continue with parsing. I need an output for testing and I can gather with a Raspberry Pi 3. 
+#BMM 10/13/2020 8:04am for an example with all outputs visit linuxoutput.txt
