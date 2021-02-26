fd83bba8c73c9a2c9c66335545b8286608280a53
uc-johns2br <johns2br@mail.uc.edu>
2021-02-26 07:13:48 -0500

    cleaned this up and added server declaration

--- astrumApp/public/busy.js
+++ astrumApp/public/busy.js
@@ -1,19 +1,4 @@
-var textWrapper = document.querySelector('.ml2');
-textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+const wsURI = `ws://localhost:8000/`;
+const ws = new WebSocket(wsURI);
 
-anime.timeline({ loop: true })
-    .add({
-        targets: '.ml2 .letter',
-        scale: [4, 1],
-        opacity: [0, 1],
-        translateZ: 0,
-        easing: "easeOutExpo",
-        duration: 950,
-        delay: (el, i) => 70 * i
-    }).add({
-        targets: '.ml2',
-        opacity: 0,
-        duration: 1000,
-        easing: "easeOutExpo",
-        delay: 1000
-    });
+window.addEventListener(ws.onmessage)
